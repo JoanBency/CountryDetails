d74af0eb723a49f90365f7344de14cf368e77ca2
Joan Bency <joanbency@yahoo.in>
2023-04-24 13:09:49 +0100

    Updated routes and controller in backend, navbar and home page done in frontend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const countryRoute = require('./Routes/country');
+
+const app = express();
+app.use(cors());
+app.use(bodyparser.json());
+
+app.use('/api/country', countryRoute);
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
