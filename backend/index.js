1608c402aadcbb2ff8687b4b63f220dc3778b16a
Joan Bency <joanbency@yahoo.in>
2023-04-28 11:08:39 +0100

    deploying to heroku

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 const cors = require('cors');
 const express = require('express');
 const bodyparser = require('body-parser');
+const path = require("path");
 
 const countryRoute = require('./Routes/country');
 
@@ -13,6 +14,10 @@ app.use(bodyparser.json());
 app.use('/api/country', countryRoute);
 
 const port = process.env.PORT || 8080;
+
+//This will create a middleware.
+//When you navigate to the root page, it would use the built react-app
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
